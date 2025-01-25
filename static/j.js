33f2695104f65b5b9295deb1b54043a0f6dd934f
unknown <nadeem.samaali@gmail.com>
2025-01-25 16:59:00 -0500

    Updated j.js

--- static/j.js
+++ static/j.js
@@ -6,11 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
         const url = urlInput.value;
 
         if (url) {
-            // Store URL in localStorage to use it on the loading page
-            localStorage.setItem("url", url);
-
-            // Redirect to the loading page
-            window.location.href = "loading.html";
+            // Send URL to the server via a POST request
+            fetch('http://127.0.0.1:5000/get_url', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ url: url }) // Send the URL as JSON
+            })
+            .then(response => response.json())
+            .then(data => {
+                // After successfully sending the URL, redirect to the loading page
+                console.log("Server Response:", data);  // Add this to check the response
+                window.location.href = "loading.html";
+            })
+            .catch(error => {
+                alert("Error sending URL to server: " + error);
+            });
         } else {
             alert("Please enter a valid URL.");
         }
