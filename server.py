1a28ae0b09f68dbb4ee6d8d4f8673ef64eec27ec
unknown <nadeem.samaali@gmail.com>
2025-01-25 15:52:59 -0500

    Updated server.py

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
+from generator import get_parsed_reponse
 
 server = Flask(__name__)
 CORS(server)
+
+url = ""
 
 @server.route('/')
 def home() :
@@ -12,6 +15,14 @@ def home() :
 def loading() :
     return
 
-@server.route('/ideas')
-def ideas() :
-    return
+@server.route('/get_url', methods=['POST'])
+def handle_json() :
+    data = request.get_json()
+
+    global url
+    url = data.get('url')
+
+@server.route('/generate_ideas')
+def generate_ideas() :
+    parsed_reponse = get_parsed_reponse(url)
+    return render_template('template1.html', json_packet = jsonify({"message" : parsed_reponse}))
