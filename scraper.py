cf465ce295dd0dd467d7f05f4ef582fddd18be28
Lina Guezi <lina.guezi@gmail.com>
2025-01-25 14:02:09 -0500

    function

--- scraper.py
+++ scraper.py
@@ -3,30 +3,31 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
-service = Service("Assets/chromedriver.exe")
+def get_title_prizes_companies(url):
+    service = Service("Assets/chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
 
-driver = webdriver.Chrome(service=service)
+    driver.get(url)
 
-driver.get('https://dawhacks2024.devpost.com/')
+    driver.get(url)
+    driver.implicitly_wait(10)
+
+    title = driver.title.split(":")[0]
+    
+
+    prizes = driver.find_elements(By.CSS_SELECTOR, ".prize-title div")
+    prize_titles = [prize.text for prize in prizes]
+
+    companies = driver.find_elements(By.CSS_SELECTOR, "article#judges i")
+    companies_text = [p.text for p in companies]
+    driver.quit()
+    return (title, prize_titles, companies_text)
+
+
+
 # driver.get("https://mcgill-physics-hackathon-2023.devpost.com/")
 # driver.get("https://mchacks-12.devpost.com/?ref_feature=challenge&ref_medium=discover")
 # driver.get("https://hack-mcwics-2025.devpost.com/?ref_feature=challenge&ref_medium=discover")
 
-driver.implicitly_wait(10)
-
-title = driver.title.split(":")[0]
-print(title)
-
-prizes = driver.find_elements(By.CSS_SELECTOR, ".prize-title div")
-
-prize_titles = [prize.text for prize in prizes]
-print(prize_titles)
-
-# for title in prize_titles:
-#     print(title)
-
-companies = driver.find_elements(By.CSS_SELECTOR, "article#judges i")
-companies_text = [p.text for p in companies]
-print(companies_text)
-
-driver.quit()
+t,p,c = get_title_prizes_companies("https://mchacks-12.devpost.com/?ref_feature=challenge&ref_medium=discover")
+print(t,p,c)
