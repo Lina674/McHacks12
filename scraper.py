d9cc1ee5188e7f77cc26db5b26162bf2131c47b2
Lina Guezi <lina.guezi@gmail.com>
2025-01-25 13:48:27 -0500

    companies

--- scraper.py
+++ scraper.py
@@ -7,10 +7,11 @@ service = Service(r"C:\Users\linag\Downloads\chromedriver-win64\chromedriver-win
 
 driver = webdriver.Chrome(service=service)
 
-# driver.get('https://dawhacks2024.devpost.com/')
+driver.get('https://dawhacks2024.devpost.com/')
 # driver.get("https://mcgill-physics-hackathon-2023.devpost.com/")
 # driver.get("https://mchacks-12.devpost.com/?ref_feature=challenge&ref_medium=discover")
-driver.get("https://hack-mcwics-2025.devpost.com/?ref_feature=challenge&ref_medium=discover")
+# driver.get("https://hack-mcwics-2025.devpost.com/?ref_feature=challenge&ref_medium=discover")
+
 driver.implicitly_wait(10)
 
 title = driver.title.split(":")[0]
@@ -24,7 +25,8 @@ print(prize_titles)
 # for title in prize_titles:
 #     print(title)
 
-judge_companies = driver.find_elements(By.CSS_SELECTOR, ".prize-title div")
-
+companies = driver.find_elements(By.CSS_SELECTOR, "article#judges i")
+companies_text = [p.text for p in companies]
+print(companies_text)
 
 driver.quit()
