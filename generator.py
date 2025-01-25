a6ac2651d58d9587726eb07c482b449239be4f6b
unknown <nadeem.samaali@gmail.com>
2025-01-25 13:34:04 -0500

    Added get_parsed_response

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 from google import generativeai as genai
+import json
 
 genai.configure(api_key="AIzaSyBQqnpIRFvNbjrZBDp1Fjgi_IsQmPpgcHU")
 
@@ -12,7 +13,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-flash-8b",
+  model_name="gemini-1.5-flash",
   generation_config=generation_config,
 )
 
@@ -22,6 +23,22 @@ chat_session = model.start_chat(
 )
 
 # Get reponse from gemini
-def get_reponse(prompt : str) -> str :
-        response = (chat_session.send_message(prompt)).text
-        return response
+def get_parsed_reponse(prompt : str, companies : list[str], prizes : list[str]) -> str :
+    response = (chat_session.send_message(prompt)).text
+    response = response.split("\n")
+    
+    response = [idea for idea in response if '|' in idea]
+    response = [idea.split("|") for idea in response]
+    response = [[element.strip().strip("*") for element in idea if element != '' ] for idea in response]
+    response.pop(1)
+
+    keys = response[0]
+    response = response[1:]
+
+    ideas_json = {"ideas_list" : []}
+    for i in range(len(response)) :
+        temp_json = {}
+        for j in range(len(keys)) :
+            temp_json[keys[j]] = response[i][j]
+        ideas_json["ideas_list"].append(temp_json)
+    return ideas_json
