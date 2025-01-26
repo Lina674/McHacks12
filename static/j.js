19a7326e8f6ccc6cc6c1a8ff2dff60e7ffd65a66
Lina Guezi <lina.guezi@gmail.com>
2025-01-26 09:30:54 -0500

    url

--- static/j.js
+++ static/j.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const submitBtn = document.getElementById("submitBtn");
     const urlInput = document.getElementById("urlInput");
-    const backendUrl = window.location.hostname === 'localhost' ? 'http://127.0.0.1:5000' : 'https://whatthehack.vercel.app';
+    const backendUrl = window.location.hostname === 'localhost' ? 'http://127.0.0.1:5000' : 'https://whatthehack.vercel.app/?';
 
     submitBtn.addEventListener("click", () => {
         const url = urlInput.value;
